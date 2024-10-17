f9b2432312e184e3e95bbca8e8ba16f146106cc8
Lexx Damm <lexx.damm@sydarkivera.se>
2024-10-17 14:20:53 +0200

    Remove unused commented-out code from index.tsx and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,3 @@
-// import React from 'react';
-// import ReactDom from 'react-dom';
-
-// const mainElement = document.createElement('div');
-// document.body.appendChild(mainElement);
-// const App = () => (
-//   <h1>
-//     Hi from a react app
-//   </h1>
-// );
-// ReactDom.render(<App />, mainElement);
-
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
@@ -64,33 +52,4 @@ class App extends Component<Props> {
   }
 }
 
-// const App = inject('errorStore')(observer(({errorStore}: Props) => (
-// <div style={{ backgroundColor: '#f7f7f7', minHeight: '100vh' }}>
-//   <Switch>
-//     <Route exact path="/" component={DBSelectScreen} />
-//     <Route exact path="/database/" component={Database} />
-//     <Route path="/database/table/" component={TablePreviewScreen} />
-//     <Route
-//       path="/database/verification/"
-//       component={VerificationScreen}
-//     />
-//     <Route path="/" component={DBSelectScreen} />
-//   </Switch>
-//   <div>
-//     {
-//       errorStore.errors.map(() => {
-//         return <article className="message is-danger">
-//         <div className="message-header">
-//           <p>Danger</p>
-//           <button className="delete" aria-label="delete"></button>
-//         </div>
-//         <div className="message-body">
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-//         </div>
-//       </article>
-//       })
-//     }
-//   </div>
-// </div>
-// )));
 export default App;
