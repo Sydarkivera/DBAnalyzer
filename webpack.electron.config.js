fa3f55070cac49f51d6a9ecdb504f53de1b259cb
Lexx Damm <lexx.damm@sydarkivera.se>
2024-10-24 11:45:47 +0200

    Add presets to babel

--- webpack.electron.config.js
+++ webpack.electron.config.js
@@ -19,6 +19,13 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
+          }
         },
       },
     ],
