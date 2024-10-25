651253cb1ae981ef944d2e55261c9c2ba45e2920
Lexx Damm <lexx.damm@sydarkivera.se>
2024-10-25 08:21:01 +0200

    Use a seperate App component from routes, because of change from switch to routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 import DBSelectScreen from './screens/DatabaseList';
 import Database from './screens/Database';
 import TablePreviewScreen from './screens/TablePreview';
-import VerificationScreen from './screens/Verification';
+import VerificationScreen from './screens/TableVerification';
 import ErrorStore from './store/ErrorStore';
 import { observer } from 'mobx-react';
 
@@ -13,39 +13,17 @@ interface Props {
   errorStore: ErrorStore
 }
 
-function Root() {
-  return (
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<DBSelectScreen />} />
-            <Route path="/database/" element={<Database />} />
-            <Route path="/database/table/" element={<TablePreviewScreen />} />
-            <Route path="/database/verification/" element={<VerificationScreen />} />
-          </Route>
-        </Routes>
-      </BrowserRouter >
+class App {
+  render() {
+    return (
+      <>
+        <header></header>
+        <body>
+        </body>
+        <footer></footer>
+      </>
     );
-  };
+  }
+};
 
-function Layout() {
-  return (
-    <>
-      <header> {/* TODO: Use header code from App.tsx version 1.0.0 */} </header>
-      <main>
-        <Outlet />
-      </main>
-      <footer> {/* TODO: Use code from App.tsx version 1.0.0 */} </footer>
-    </>
-  );
-}
-
-// Router singleton created
-const router = createBrowserRouter([
-  { path: "*", element: <Root /> },
-]);
-
-// RouterProvider added
-export default function App() {
-  return <RouterProvider router={router} />;
-}
+export default App;
