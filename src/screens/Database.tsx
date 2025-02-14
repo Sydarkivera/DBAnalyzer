a03f02bf05a9419f46e3d34b12881ae2130f86c8
Lexx Damm <lexx.damm@sydarkivera.se>
2025-02-14 14:00:40 +0100

    fix linux/windows/git lower/uppercase on files

--- src/screens/Database.tsx
+++ src/screens/Database.tsx
@@ -6,7 +6,7 @@ import { observer, inject } from 'mobx-react';
 import { FaSearch } from 'react-icons/fa';
 import ErrorStore from 'src/store/ErrorStore';
 import ConnectionModal from '../components/ConnectionModal';
-import ProgressBar, { VisualPart } from '../components/progressBar';
+import ProgressBar, { VisualPart } from '../components/ProgressBar';
 import SelectedStore from '../store/Selected';
 import ShouldSaveButton from '../components/ShouldSaveButton';
 import TableStore, { ShouldSave } from '../store/Table';
