a90fcb6b5434b47a37780bed740b5225332f516b
Lexx Damm <lexx.damm@sydarkivera.se>
2025-02-14 13:42:52 +0100

    add react-router-dom package. Remove unused files. Rename lin/win mix

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,10 @@ import { Provider } from 'mobx-react';
 import './index.css';
 import App from './App';
 import stores from './store';
+/* 
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import '@creativebulma/bulma-tooltip/dist/bulma-tooltip.min.css';
+*/
 
 const root = createRoot(document.body);
 root.render(
--- src/screens/Verification.tsx
+++ src/screens/Verification.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import { observable } from 'mobx';
 import { observer, inject } from 'mobx-react';
 import '../App.css';
